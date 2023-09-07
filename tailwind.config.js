6c3ab546be17f59023812fb8ac6d302935dd232c
sawfiz <dashu.up@gmail.com>
2023-09-07 18:10:34 -0400

    Add dark mode toggle switch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/**/*.ejs"],
-  darkMode: 'media',
+  // darkMode: 'media', // darkMode depend on system setting
+  darkMode: 'class',  // allows the dark mode toggle switch to work
   mode: "jit",
   theme: {
     extend: {},
