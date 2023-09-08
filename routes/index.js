bd83d116cd4122ea5b6359b70483f250de9b68aa
sawfiz <dashu.up@gmail.com>
2023-09-08 09:47:09 -0400

    Optimize index.js

--- routes/index.js
+++ routes/index.js
@@ -9,25 +9,40 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 const Message = require('../models/message');
 
-// Wait for database to connect, logging an error if there is a problem
-async function main() {
-  await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log('database connected');
-
-  router.get('/', async function (req, res, next) {
-    try {
-      const messages = await readCollectionToArray();
-      res.render('index', { title: 'Mini Message Board', messages: messages });
-    } catch (error) {
-      next(error); // Pass the error to the error handling middleware
-    }
-  });
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Database connection error:', error);
+  }
 }
 
-main().catch((err) => console.log(err));
+// Connect to the database when the app starts
+connectToDatabase();
+
+// Wait for database to connect, logging an error if there is a problem
+// async function main() {
+//   await mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   });
+//   console.log('database connected');
+
+//   router.get('/', async function (req, res, next) {
+//     try {
+//       const messages = await readCollectionToArray();
+//       res.render('index', { title: 'Mini Message Board', messages: messages });
+//     } catch (error) {
+//       next(error); // Pass the error to the error handling middleware
+//     }
+//   });
+// }
+
+// main().catch((err) => console.log(err));
 
 // Import the Message model
 
@@ -37,16 +52,12 @@ async function readCollectionToArray() {
     const documents = await Message.find().lean(); // Use .lean() for plain JavaScript objects
 
     // Map the collection into an array of objects
-    const dataArray = documents.map((document) => {
-      return {
+    const dataArray = documents.map((document) => ({
         _id: document._id,
         user: document.user,
         text: document.text,
         added: document.added,
-      };
-    });
-
-    console.log("🚀 ~ file: index.js:55 ~ readCollectionToArray ~ dataArray:", dataArray)
+    }));
     return dataArray;
   } catch (error) {
     console.error('Error reading collection:', error);
@@ -59,7 +70,9 @@ async function readCollectionToArray() {
 router.get('/', async function (req, res, next) {
   try {
     const messages = await readCollectionToArray();
-    res.render('index', { title: 'Mini Message Board', messages: messages });
+    // Reverse the order of the messages array
+    const reversedMessages = messages.reverse();
+    res.render('index', { title: 'Mini Message Board', messages: reversedMessages });
   } catch (error) {
     next(error); // Pass the error to the error handling middleware
   }
@@ -68,17 +81,14 @@ router.get('/', async function (req, res, next) {
 router.post('/', async function (req, res, next) {
   console.log(req.body);
   const { user, text } = req.body;
-
-  const newMsg = new Message({user, text, added: new Date()})
-  await newMsg.save();
-
-  // messages.push({
-  //   text,
-  //   user,
-  //   added: new Date(),
-  // });
-
-  res.redirect('/');
+  try {
+    const newMsg = new Message({ user, text, added: new Date() });
+    await newMsg.save();
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error saving message:', error);
+    next(error);
+  }
 });
 
 module.exports = router;
